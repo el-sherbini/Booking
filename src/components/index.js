dba7dd67c536b9e471d1b1a926e2c5190c5726da
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-06-10 19:13:29 +0200

    add PropertyList component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Navbar } from "./navbar/Navbar";
 export { default as Header } from "./header/Header";
 export { default as Featured } from "./featured/Featured";
+export { default as PropertyList } from "./propertyList/PropertyList";
 export { default as Footer } from "./footer/Footer";
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import { Featured, Header, Navbar } from "../../components";
+import { Featured, Header, Navbar, PropertyList } from "../../components";
 import "./home.scss";
 
 const Home = () => {
@@ -8,6 +8,8 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
       </div>
     </div>
   );
