2bee664d66cee06d3b875b7d67d2ce75c0db3b98
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-06-11 20:02:04 +0200

    add room model, routes and controllers

---
+++ server/controllers/room.js
@@ -0,0 +1,75 @@
+import Room from "../models/Room.js";
+import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+
+// Create a new room
+export const createRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  const newRoom = new Room(req.body);
+
+  try {
+    const savedRoom = await newRoom.save();
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $push: { rooms: savedRoom._id },
+      });
+    } catch (err) {
+      next(err);
+    }
+    res.status(200).json(savedRoom);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Update existing room
+export const updateRoom = async (req, res, next) => {
+  try {
+    const updatedRoom = await Room.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedRoom);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Delete existing room
+export const deleteRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  try {
+    await Room.findByIdAndDelete(req.params.id);
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $pull: { rooms: req.params.id },
+      });
+    } catch (err) {
+      next(err);
+    }
+    res.status(200).json("Room has been deleted.");
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get room by id
+export const getRoom = async (req, res, next) => {
+  try {
+    const room = await Room.findById(req.params.id);
+    res.status(200).json(room);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get all rooms
+export const getRooms = async (req, res, next) => {
+  try {
+    const rooms = await Room.find();
+    res.status(200).json(rooms);
+  } catch (err) {
+    next(err);
+  }
+};
--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,5 +1,22 @@
 import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+router.put("/:id", verifyAdmin, updateRoom);
+
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+router.get("/:id", getRoom);
+router.get("/", getRooms);
 
 export default router;
