4f9e942ad8000df75de8f497c15f8f167b8c5db5
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-06-11 14:48:21 +0200

    prepare back-end structure and add MongoDB URI

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+app.listen(5000, () => {
+  connect();
+  console.log("Server is running on port 5000");
+});
